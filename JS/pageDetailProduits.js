e0681efb6cc0d0585d549c4add9143b8bfc46d5a
Diarassouba Mamadou <119653418+CoudaCode@users.noreply.github.com>
2024-08-01 00:08:51 +0000

    modifier login

---
+++ JS/pageDetailProduits.js
@@ -0,0 +1,39 @@
+// pageDetailsProduict.js
+window.addEventListener("DOMContentLoaded", function () {
+  // const urlParams = new URLSearchParams(window.location.search);
+  // const serviceId = urlParams.get("id");
+  // if (serviceId) {
+  //   fetchServiceDetails(serviceId);
+  // } else {
+  //   console.error("No service ID found in URL");
+  // }
+  // async function fetchServiceDetails(id) {
+  //   try {
+  //     const response = await fetch(`${BASE_URL}/service/${id}`, {
+  //       method: "GET",
+  //     });
+  //     const result = await response.json();
+  //     console.log("result", result);
+  //     if (response.ok) {
+  //       renderServiceDetails(result.data);
+  //     } else {
+  //       console.error(result.message);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+  // function renderServiceDetails(service) {
+  //   const serviceContainer = document.getElementById("service-details");
+  //   if (serviceContainer) {
+  //     serviceContainer.innerHTML = `
+  //       <h1>${service.name}</h1>
+  //       <img src="${service.images[0]}" alt="${service.name}" />
+  //       <p>${service.description}</p>
+  //       <p>Price: ${service.price} FCFA</p>
+  //       <p>Artisant: ${service.artisant.user.firstname} ${service.artisant.user.lastname}</p>
+  //       <p>Job: ${service.artisant.job}</p>
+  //     `;
+  //   }
+  // }
+});
--- JS/pageProduit.js
+++ JS/pageProduit.js
@@ -1,23 +1,14 @@
 const BASE_URL = "http://localhost:3000/api";
 
-// const token = localStorage.getItem("accessToken");
-// let profile = document.querySelector(".profile");
-// window.addEventListener("DOMContentLoaded", async function () {
-//   if (!token) {
-//     window.location.href = "http://127.0.0.1:5500/HTML/login.html";
-//   }
-
-//   console.log("token", token);
-//   console.log("profile", profile);
-// });
-
 window.addEventListener("DOMContentLoaded", function () {
   const token = localStorage.getItem("accessToken");
+  const servicesContainer = document.getElementById("services");
 
   const sellButton = document.querySelector(".sell-button");
   const buyButton = document.querySelector(".buy-button");
   const profileIcon = document.getElementById("profile-icon");
 
+  console.log("token", token);
   if (token) {
     // Si connecté, afficher l'icône de profil et masquer les boutons "Vendre" et "Acheter"
     if (profileIcon) profileIcon.classList.remove("hidden");
@@ -32,7 +23,73 @@ window.addEventListener("DOMContentLoaded", function () {
   if (logoutButton) {
     logoutButton.addEventListener("click", function () {
       localStorage.removeItem("accessToken");
-      window.location.reload(); // Recharger la page pour mettre à jour l'état de connexion
+      window.location.reload();
     });
   }
+
+  // Affichage des Produits
+  const fetchServices = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/service/`, {
+        method: "GET",
+      });
+
+      const result = await response.json();
+
+      console.log("result", result);
+      if (response.ok) {
+        renderServices(result.data);
+      } else {
+        console.error(result.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const renderServices = (services) => {
+    servicesContainer.innerHTML = ""; // Clear previous services
+    services.forEach((service) => {
+      console.log("service", service);
+      const serviceCard = `
+        <div class="bg-white shadow-md rounded-lg p-4">
+          <a href="./../Acheteur/page_service_detaille.html">
+            <img
+              src="${service.images[0]}" // Assuming the service object has an imageUrl property
+              alt="${service.name}"
+              class="w-full h-64 object-contain object-center rounded-t-lg"
+          />
+          <div class="mt-4">
+            <div class="flex justify-between mb-2">
+              <h2 class="text-gray-800">${service.name}</h2>
+              <div class="flex">
+                <i class="fas fa-star text-yellow-500"></i>
+                <i class="fas fa-star text-yellow-500"></i>
+                <i class="fas fa-star text-yellow-500"></i>
+                <i class="far fa-star text-yellow-500"></i>
+                <i class="far fa-star text-yellow-500"></i>
+              </div>
+            </div>
+            <div class="flex items-center mt-4 justify-between">
+              <div class="flex items-center">
+                <div
+                  class="h-8 flex items-center shadow-rounded justify-center"
+                >
+                  <i class="fas fa-user-circle text-black text-3xl mr-2"></i>
+                </div>
+                <div class="ml-2">
+                  <span class="text-sm">${service.artisant.user.firstname} ${service.artisant.user.lastname}</span>
+                  <p class="text-xs text-gray-500">${service.artisant.job}</p>
+                </div>
+              </div>
+              <p class="text-lg font-semibold">${service.price} FCFA</p>
+            </div>
+          </div>
+          </a>
+        </div>`;
+      servicesContainer.insertAdjacentHTML("beforeend", serviceCard);
+    });
+  };
+
+  fetchServices();
 });
